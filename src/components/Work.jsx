5683bbc01944918a2b533ee73aba7598c6567984
ashish30704 <ashish30704@gmail.com>
2025-06-22 11:52:52 +0530

    made a few improvements

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -21,7 +21,7 @@ const Work = () => {
         <h2 className="headline-2 mb-8">
           My Work and Experience
         </h2>
-        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(450px,_1fr))]">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           {
             WorksList.map(({title, type, location, company, duration, description, logo, certification}, key) => (
               <InternshipCard
